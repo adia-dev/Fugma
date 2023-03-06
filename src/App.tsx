a812bf6890512f2b9241b5110723485c6c4e87a2
Abdoulaye DIA <abdoudu78130@gmail.com>
2023-03-06 23:20:51 +0100

    Batman
    - Initialized the project with vite react and the typescript template
    - Installed
        - react-icons
        - tailwindCSS
        - yjs
        - y-webrtc
        - y-websocket
        - y-indexeddb

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  return <h1 className="text-3xl font-bold underline">Hello World !</h1>;
+}
+
+export default App;
